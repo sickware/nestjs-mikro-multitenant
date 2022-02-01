9489a1b45986774589106ccef25a948d9c47b225
Rafael Ledesma <rledesma@softengy.com>
2022-02-01 16:17:36 -0600

    relaciones

--- src/database/models/addRelations.ts
+++ src/database/models/addRelations.ts
@@ -1,8 +1,21 @@
-export const addRelations = ( tablaPrimary : string, tablaForeign : string ) : string  => {
+/**
+ * schemaPrimary = $empresa || $sucursal || $public
+ * tablaPrimary = Organizacion -> id || uuid
+ * 
+ * schemaForeign = $empresa || sucursal || public 
+ * tablaForeign = Cliente -> id_organizacion
+ * 
+ * 
+ * podria enviarse separado (tenant_12546, organizacion, )
+ * o todo junto (tenant123154.organizacion,)./
+ */
 
-    /**
-     * tablaPrimary = Organizacion -> id || uuid
-     * tablaForeign = Cliente -> id_organizacion
-     */
+export const addRelations = ( tablaPrimary : string, tablaForeign : string ) : string  => {
+    
+    const query = `alter table NOMBRETABLA1
+    add constraint NOMBRERESTRICCION
+    foreign key (CAMPOCLAVEFORANEA)
+    references NOMBRETABLA2 (CAMPOCLAVEPRIMARIA);`
+
     return 'sd'
 }
