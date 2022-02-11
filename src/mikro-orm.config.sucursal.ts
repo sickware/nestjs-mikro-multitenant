c67527dc88d3dac3cd43e4a1bd4b668f9da6332e
Rafael Ledesma <rledesma@softengy.com>
2022-02-11 13:10:58 -0600

    Archivos de configuracion modificados

--- src/mikro-orm.config.sucursal.ts
+++ src/mikro-orm.config.sucursal.ts
@@ -3,13 +3,8 @@ import { TsMorphMetadataProvider } from '@mikro-orm/reflection';
 import * as Path from 'path';
 
 const configSucursal : Options = {
-    type : 'postgresql',
-    dbName : 'multitenant',
-    user : 'postgres',
-    password : '13051997ec',
     entities : [ 'dist/database/models/sucursal/*.entity.js' ],
     entitiesTs : [ 'src/database/models/sucursal/*.entity.ts' ],//podemos especificar las rutas *no es necesario
-    metadataProvider : TsMorphMetadataProvider,
     migrations : {
         path : 'dist/database/migrations/sucursal',
         pathTs : Path.join( __dirname, './database/migrations/sucursal'),
