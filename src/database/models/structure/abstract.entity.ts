4a960f205c90d339d69a9ac612cbbb5c9929d75a
Rafael Ledesma <rledesma@softengy.com>
2022-02-21 12:01:23 -0600

    entity abstract -> timestamp agregado

--- src/database/models/structure/abstract.entity.ts
+++ src/database/models/structure/abstract.entity.ts
@@ -4,10 +4,10 @@ export abstract class AbstractEntity {
     @PrimaryKey({ type : 'uuid', defaultRaw : 'uuid_generate_v4()' })
     uuid! : string;
     
-    @Property({ type : 'timestamps' })
+    @Property({ type : 'timestamp' })
     createdAt = new Date();
 
-    @Property({ type : 'timestamps', onUpdate : () => new Date() })
+    @Property({ type : 'timestamp', onUpdate : () => new Date() })
     updatedAt = new Date();
 
 }
