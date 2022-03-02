12159bd5e1564db82d56bf56cce1963129ff44bf
Rafael Ledesma <rledesma@softengy.com>
2022-03-02 13:35:53 -0600

    Wrap a entidad company y populate - populate no funciono

--- src/test-2776/test-2776-2.service.spec.ts
+++ src/test-2776/test-2776-2.service.spec.ts
@@ -21,8 +21,8 @@ describe('Test2776Service', () => {
     })
 
     await orm.getSchemaGenerator().refreshDatabase();
-    await orm.getSchemaGenerator().updateSchema({ schema : 's2'});
-    await orm.em.nativeDelete(Company,{},{ schema : 's2' });
+    await orm.getSchemaGenerator().updateSchema({ schema : 's1'});
+    await orm.em.nativeDelete(Company,{},{ schema : 's1' });
 
     orm = await MikroORM.init({
       entities : [ CustomerStructure ],
@@ -34,8 +34,8 @@ describe('Test2776Service', () => {
     })
 
     await orm.getSchemaGenerator().refreshDatabase();
-    await orm.getSchemaGenerator().updateSchema({ schema : 's3'});
-    await orm.em.nativeDelete(Customer,{},{ schema : 's3' });
+    await orm.getSchemaGenerator().updateSchema({ schema : 's2'});
+    await orm.em.nativeDelete(Customer,{},{ schema : 's2' });
 
     orm = await MikroORM.init({
       entities : [ Company, Customer ],
@@ -43,7 +43,8 @@ describe('Test2776Service', () => {
       type : 'postgresql',
       user : 'postgres',
       password : '13051997ec',
-      allowGlobalContext : true
+      allowGlobalContext : true,
+      debug : true
     })
 
   });
@@ -53,18 +54,27 @@ describe('Test2776Service', () => {
   });
 
   test('wildcard entities', async () => {
-    const c = new Customer;
+    const c = new Customer();
     c.name = 'e';
     c.company = new Company();
     c.company.name = 'c';
-    wrap(c).setSchema('s3');
-    wrap(c.company).setSchema('s2');
+    wrap(c).setSchema('s2');
+    wrap(c.company).setSchema('s1');
     await orm.em.fork().persistAndFlush(c);
-    const res = await orm.em.getRepository(Customer).findAll({ populate : true, schema : 's3' });
-    expect(res).toHaveLength(1);
-    expect(wrap(res[0].company).isInitialized()).toBe(true);
-    expect( res[0].name ).toBe('e');
-    expect( res[0].company.name).toBe('c');
+    const [customer] = await orm.em.getRepository(Customer).findAll({ schema : 's2' });
+    wrap(customer).setSchema('s2');
+    wrap(customer.company).setSchema('s1');
+    // const resp = await orm.em.populate( customer, true );
+    // const resp = await wrap(customer).populated(true);
+    const resp = await wrap(customer).populated(true);
+    console.log(resp);
+
+    expect( wrap(customer.company).getSchema() ).toBe('s1');
+    expect( wrap(customer).getSchema() ).toBe('s2');
+    expect(customer.name).toBe('e');
+    expect( wrap(customer.company).isInitialized() ).toBe(true);
+    expect(customer.company.name).toBe('c');
+    
   })
 
 });
